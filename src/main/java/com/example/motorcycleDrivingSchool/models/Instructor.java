37c4a91e78199ca22e176890c34afc63e64bc27b
Tomasz-asus <81529362+Tomasz-asus@users.noreply.github.com>
2022-10-04 10:42:57 +0200

    Initial commit

---
+++ src/main/java/com/example/motorcycleDrivingSchool/models/Instructor.java
@@ -0,0 +1,4 @@
+package com.example.motorcycleshop.models;
+
+public class Instructor {
+}
---
+++ src/main/java/com/example/motorcycleDrivingSchool/models/Producent.java
@@ -0,0 +1,69 @@
+package com.example.motorcycleDrivingSchool.models;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Category {
+    @Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private String imgUrl;
+
+
+    public Category() {
+    }
+
+    public Category(String name, String description, String imgUrl) {
+        this.name = name;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
